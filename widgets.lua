dfa2bf41c6b1a55fb6799966b76fbaf50c948f4d
Andrew Teplyashin <platinumthinker@gmail.com>
2014-03-21 12:34:13 -0400

    Swap cpu widget and battery widget

--- widgets.lua
+++ widgets.lua
@@ -57,8 +57,8 @@ for s = 1, screen.count() do
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
-    right_layout:add(batwidget)
     right_layout:add(cpuwidget)
+    right_layout:add(batwidget)
     if s == 1 then right_layout:add(wibox.widget.systray()) end
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
