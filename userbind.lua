2b98a5340cff897479285a26fd31686411f7906b
Andrey Teplyashin <platinumthinker@gmail.com>
2017-01-29 17:50:14 +0700

    Fix to work

--- userbind.lua
+++ userbind.lua
@@ -5,9 +5,9 @@ dmenu = "dmenu_run -fn 'DejaVu Mono-11:normal' -nb '#002b36' -nf '#657b83' -sb '
 -- {{{ Key bindings
 globalkeys = awful.util.table.join(globalkeys,
     awful.key({ modkey,         }, "g",
-        function () awful.util.spawn("dolphin") end),
+        function () awful.util.spawn("thunar") end),
     awful.key({ modkey,         }, "i",
-        function () awful.util.spawn("iceweasel") end),
+        function () awful.util.spawn("firefox") end),
     awful.key({ modkey,         }, "d",
         function () awful.util.spawn(dmenu) end),
     awful.key({ modkey, "Shift" }, "w",
