1bbbd55385a58185811c35d0a1b98d785b55e258
platinumthinker <platinumthinker@gmail.com>
2017-04-01 15:10:54 +0700

    Delete unused windows layouts

--- layouts.lua
+++ layouts.lua
@@ -5,15 +5,15 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
-layouts =
+awful.layout.layouts =
 {
     awful.layout.suit.corner.nw,
     awful.layout.suit.floating,
     awful.layout.suit.tile,
     -- awful.layout.suit.tile.bottom,
-    awful.layout.suit.fair,
-    -- awful.layout.suit.fair.horizontal,
-    awful.layout.suit.spiral.dwindle
+    -- awful.layout.suit.fair,
+    awful.layout.suit.fair.horizontal,
+    -- awful.layout.suit.spiral.dwindle
     -- awful.layout.suit.max,
     -- awful.layout.suit.magnifier
 }
